981eedc241cc680c5dabe9eac5f2e03c2a7f330b
Brenda Alvis <brendaalvis@brendas-mbp-2.lan>
2022-09-09 15:48:22 -0500

    profile styling

--- Public/js/profile.js
+++ Public/js/profile.js
@@ -1,7 +1,7 @@
 const newFormHandler = async (event) => {
     event.preventDefault();
   
-    const title = document.querySelector('#project-name').value.trim();
+    const title = document.querySelector('#project-title').value.trim();
     const description = document.querySelector('#project-desc').value.trim();
   
     if (title && description) {
